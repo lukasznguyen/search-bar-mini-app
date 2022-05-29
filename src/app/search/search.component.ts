26966586aedfe015ec40f654261ea8a6b6bff376
Tuan Viet Nguyen <lukasznguyen1999@gmail.com>
2022-05-29 13:05:26 +0200

    Added search bar with autocomplete presentation

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -8,14 +11,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SearchComponent implements OnInit {
 
-  searchDictionary: any;
+  searchDictionary: string[] = [];
+  myControl = new FormControl();
+  filteredOptions!: Observable<string[]>;
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     this.httpClient.get("assets/data.json").subscribe(data => {
-      this.searchDictionary = data;
+      this.searchDictionary = data.toString().split(',').sort();
     });
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.searchDictionary.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
-
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     SearchRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
   ]
 })
 export class SearchModule { }
